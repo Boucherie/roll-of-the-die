be29df02afccd98ff59a4a9020e0e58a3a218281
Emily Boucher <emi.boucher@gmail.com>
2017-05-22 22:50:59 -0400

    finsihed odds

--- odds.rb
+++ odds.rb
@@ -1,16 +1,34 @@
 def dice_roller2
   count = 0
-  count_totals = []
+  totals = []
   (1..6).each do |roll1|
     (1..6).each do |roll2|
-      count_sum = roll1 + roll2
-      count += 1
-      count_totals << count_sum
+      totals << roll1 + roll2
     end
   end
-  count_totals.each do |total|
+
+  occurences = {
+    2 => 0,
+    3 => 0,
+    4 => 0,
+    5 => 0,
+    6 => 0,
+    7 => 0,
+    8 => 0,
+    9 => 0,
+    10 => 0,
+    11 => 0,
+    12 => 0
+  }
+
+  totals.each do |total|
+    occurences[total] += 1
+  end
+
+  occurences.each do |roll, occurence|
+    puts "The odds of  #{roll} coming up are #{((occurence.to_f / 36) * 100).round }% "
+  end
 
 end
 
-dice_roller2
-# show odds of number appearing
+puts dice_roller2
